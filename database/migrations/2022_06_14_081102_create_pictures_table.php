a5f1df96a5c728fde962b29631d9d3175a100d0d
Estelle <estellegregoire0689@gmail.com>
2022-06-14 10:38:46 +0200

    updates

--- database/migrations/2022_06_14_081102_create_pictures_table.php
+++ database/migrations/2022_06_14_081102_create_pictures_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('pictures', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('author',55);
+            $table->text('description');
+            $table->string('title',255);
+            $table->string('url',255);
+            $table->string('like');
+            $table->timestamps(); //generate createdat and updatedat
         });
     }
 
